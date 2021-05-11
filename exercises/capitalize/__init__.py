b5eb3c55c90bb37a5693e940c5b35c1f44a5b284
koushikvikram <gridfinsdeploy@gmail.com>
2021-05-11 17:10:57 +0530

    stubs

---
+++ exercises/capitalize/__init__.py
@@ -0,0 +1,13 @@
+# --- Directions
+# Write a function that accepts a string.  The function should
+# capitalize the first letter of each word in the string then
+# return the capitalized string.
+# --- Examples
+#   capitalize('a short sentence') --> 'A Short Sentence'
+#   capitalize('a lazy fox') --> 'A Lazy Fox'
+#   capitalize('look, it is working!') --> 'Look, It Is Working!'
+
+
+def capitalize():
+    raise NotImplementedError
+    

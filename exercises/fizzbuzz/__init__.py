ecb6f5bb72328b470b32408a1a86e5fc904ae3e0
koushikvikram <gridfinsdeploy@gmail.com>
2021-05-12 09:56:21 +0530

    done

--- exercises/fizzbuzz/__init__.py
+++ exercises/fizzbuzz/__init__.py
@@ -12,5 +12,19 @@
 #   4
 #   buzz
 
-def fizzBuzz():
-    raise NotImplementedError
+
+def fizzBuzz(n: int) -> None:
+    if n < 1:
+        raise Exception("Input should be greater than 0")
+    for i in range(1, n+1):
+        if i%15 == 0:
+            print("fizzbuzz")
+        elif i%3 == 0:
+            print("fizz")
+        elif i%5 == 0:
+            print("buzz")
+        else:
+            print(i)
+
+
+fizzBuzz(31)
